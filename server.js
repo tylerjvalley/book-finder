cd8bddf31e1a73c9426973111402b975046e74de
Tyler Valley <tvalley93@gmail.com>
2019-09-11 17:10:40 -0500

    Edit server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`, this.address().port, app.settings.env);
+    console.log(`server started on port ${port}`);
 })
 
 
